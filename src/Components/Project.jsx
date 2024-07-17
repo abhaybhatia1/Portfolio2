3513a265a518f31ff8fbc9c61d0b4394617306cb
abhaybhatia1 <97758395+abhaybhatia1@users.noreply.github.com>
2024-07-18 00:51:24 +0530

    Update Project.jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -41,7 +41,7 @@ const projectsData = [
 
 function Project() {
   return (
-    <div className="container mx-auto py-8">
+    <div className="container mx-auto p-8">
       <h1 className="text-3xl font-semibold text-center mb-8">Projects</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {projectsData.map((project, index) => (
