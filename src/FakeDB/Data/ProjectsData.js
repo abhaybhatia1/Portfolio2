b2c5f892f5020bbf6a0d2f1ec6e155bb363ff0e4
abhaybhatia1 <97758395+abhaybhatia1@users.noreply.github.com>
2024-08-01 14:23:43 +0530

    Update ProjectsData.js

--- src/FakeDB/Data/ProjectsData.js
+++ src/FakeDB/Data/ProjectsData.js
@@ -21,31 +21,10 @@ const ProjectsData = [
       ],
     },
     {
-      title: 'Internal Dev Platform',
-      summary: 'This project helps to show the Environments running and Releases in a company transparently.',
-      sourceCodeLink: 'https://github.com/abhaybhatia1/Employee-Management-System',
-      deployedLink: 'https://project2.example.com',
-      skillsUsed: ['React js', 'GIT', 'AWS S3 Bucket'],
-      sourceCodeExplanation: [
-        {
-          heading: "Overview",
-          content: "The Internal Dev Platform displays company environments and releases transparently.",
-        },
-        {
-          heading: "Technologies Used",
-          content: "React.js, GIT, and AWS S3 Bucket were used for development.",
-        },
-        {
-          heading: "Additional Info",
-          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        },
-      ],
-    },
-    {
       title: 'Input Validation System',
       summary: 'Express.js backend validates order and vehicle data (.xlsx, .csv), React.js frontend simplifies submission and ensures formatting adherence.',
       sourceCodeLink: 'https://github.com/user/project3',
-      deployedLink: 'https://project3.example.com',
+      deployedLink: '',
       skillsUsed: ['React js', 'Express js', 'Tailwind CSS', 'Postman'],
       sourceCodeExplanation: [
         {
